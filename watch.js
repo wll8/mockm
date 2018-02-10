1a412b19f0983e8e2285f6bc7577959f0cc9653f
wll8 <xwdaysme@gmail.com>
2018-02-10 22:52:09 +0800

    读取表及 id

--- watch.js
+++ watch.js
@@ -10,10 +10,31 @@ module.exports = (dbname, port) => {
     console.log('访问 ', req.originalUrl)
     // 去除url前后的/，并分割为数组 /a/b/c => ['a', 'b', 'c']
     let pathname = url.parse(req.originalUrl).pathname.replace(/^\//,'').replace(/$\//,'').split('/')
-    let database = jsonfile.readFileSync(dbname)
-    let tablename = pathname[0] // 读取到的 json
-    let id = pathname[1] // 取得 /blog/1 中的 blog
-    res.send(database[tablename]) // 发送数据
+    let database = jsonfile.readFileSync(dbname) // 读取到的 json
+    let tablename = pathname[0] // 取得 /blog/1 中的 blog
+    if(!database[tablename]) {
+      return res.send({ error: `表${tablename}数据未找到` })
+    }
+    let id = pathname.length > 1 ? pathname[1] : -1 // 当有 id 时取得 id
+    let get =  req.method === 'GET'
+    let getAll = (get && id === -1) // 没有 id 时获取整张表
+    let getOne = (get && id > -1)
+    let post = req.method === 'POST'
+    let patch = req.method === 'PATCH'
+    let Delete = req.method === 'DELETE'
+    let put = req.method === 'PUT'
+
+    if(getAll){
+      res.send(database[tablename]) // 没有 id 时发送整张表数据
+    }
+    if(getOne){
+      let data = database[tablename]
+      let obj = data.find(obj => { // filter 返回的是数组 find 是对象
+        return obj.id == id
+      })
+      obj = obj || { error: `id${id}数据未找到` }
+      res.send(obj) // 发送数据
+    }
   })
   app.listen(port, () => {
     console.log('服务已经启动 ', port)
