f7e8107d66862666f9c66c90dffe47f57b2b0951
wll8 <xwdaysme@gmail.com>
2020-01-15 22:11:21 +0800

    feat: 录制转发的请求与响应

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const config = {
   prot: 9000, // 本地端口
   proxyTag: 't', // 测试标志, 若不匹配 /api/t/* 即进行转发
+  apiTest: 'test', // api 调试地址, 即 /api/t/test/* 进入接口调试
   proxyTarget: 'http://192.168.6.59:9000/', // 转发URL
 }
 
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const modifyResponse = require('node-http-proxy-json')
+const {htmlEscape} = require('escape-goat')
 const config = require('./config.js')
 const preFix = 'api' // api 地址前缀
 const proxy = require('http-proxy-middleware')
@@ -5,47 +7,83 @@ const jsonServer = require('json-server')
 const fs = require('fs')
 const server = jsonServer.create()
 const db = require('./db.js')()
-fs.writeFileSync('./db.json', JSON.stringify(db, null, 2))
+fs.writeFileSync('./db.json', o2t(db))
 const router = jsonServer.router('./db.json')
 const middlewares = jsonServer.defaults({bodyParser: true})
+const httpHistory = require('./httpHistory.json') // 请求历史
+const querystring = require('querystring')
+const middlewaresObj = middlewares.flat().reduce((res, item) => {
+  // 使用 jsonServer 里面的中间件, 以保持一致:
+  // compression, corsMiddleware, serveStatic, logger, jsonParser, urlencodedParser
+  return ({
+    ...res,
+    [item.name]: item,
+  })
+}, {})
 const multiparty = require('multiparty')
 let TOKEN = ''
-let CMD = `
-  curl http://www.httpbin.org/get
-`
+// server.use(middlewaresObj.corsMiddleware)
 
 server.use(proxy(
-  pathname => (Boolean(pathname.match(`/${preFix}/t/`)) === false),
+  pathname => (Boolean(pathname.match(`/${preFix}/${config.proxyTag}/`)) === false),
   {
     target: config.proxyTarget,
     changeOrigin: true,
     onProxyReq: (proxyReq, req, res) => {
+      middlewaresObj.jsonParser(req, res, () => {
+        const {
+          method,
+          url,
+        } = req
+        httpHistory[`${method} ${url}`] = {
+          req: {
+            headers: req.headers,
+            body: req.body,
+            params: req.params,
+            query: req.query,
+          },
+        }
+      })
       TOKEN = req.get('Authorization') || TOKEN // 获取 token
     },
     onProxyRes: (proxyRes, req, res) => {
-      if(req.method === 'OPTIONS') {
-        // // 配置跨域跨域之后, 可以直接在浏览器控制台使用 fetch 测试 api, 但是如果配置没有与后端一致, 则导致模拟不统一
-        // proxyRes.headers['Access-Control-Allow-Origin'] = req.headers.origin
-        // proxyRes.headers['Access-Control-Allow-Credentials'] = true
-        // proxyRes.headers['Access-Control-Allow-Methods'] = 'PUT, GET' // 允许的方法列表
-        // proxyRes.headers['Access-Control-Allow-Headers'] = 'authorization, cache-control' // 允许的自定义 headers
-        // proxyRes.headers['Access-Controll-Max-Age'] = '1728000' // 在些时间内不需再申请跨域
-      }
+      const {
+        method,
+        url,
+      } = req
+      modifyResponse(res, proxyRes, body => {
+        if( // 这些路由不记录
+          method.match(/OPTIONS/i)
+          || (
+            method.match(/GET/i) && url.match(new RegExp(`/\/api\/${config.proxyTag}\/${config.apiTest}/`))
+          )
+        ) {
+          return body
+        }
+        httpHistory[`${method} ${url}`] = {
+          ...httpHistory[`${method} ${url}`],
+          res: {
+            headers: proxyRes.headers,
+            body,
+          }
+        }
+        fs.writeFileSync(`./httpHistory.json`, o2t(httpHistory))
+        // console.log('httpHistory', o2t(httpHistory))
+        return body
+      })
     },
   },
 ))
 
-server.use(middlewares)
-
 server.use(jsonServer.rewriter({ // 修改路由, 方便后面的 api 书写
   [`/${preFix}/${config.proxyTag}/*`] : '/$1',
 }))
+server.use(middlewares)
 server.use((req, res, next) => { // 修改分页参数, 符合项目中的参数
   req.query.page && (req.query._page = req.query.page)
   req.query.pageSize && (req.query._limit = req.query.pageSize)
   next()
 })
-
 server.post('/file/upload', (req, res, next) => { // 上传文件
   const form = new multiparty.Form()
   form.parse(req, (err, fields = [], files) => {
@@ -54,80 +92,78 @@ server.post('/file/upload', (req, res, next) => { // 上传文件
   })
 })
 
-server.get('/test', (req, res, next) => { // 给后端查询前端请求的接口
-  let cmd = `${CMD} -s -l -v -g`
-    .replace(/\n/g, '')
-    .trim()
-  if(TOKEN) {
-    cmd = cmd.replace(/(Authorization: ).*?'/, `$1${TOKEN}'`)
+server.get(`/${config.apiTest}`, (req, res, next) => { // 给后端查询前端请求的接口
+  const {api} = req.query
+  if(!api) {
+    res.type('html')
+    res.send(Object.keys(httpHistory).map(item => `<a href="/api/${config.proxyTag}/${config.apiTest}?api=${querystring.escape(item)}">${item}</a>`).join('<br />'))
+    return
   }
-  let jsonInfo = {obj: {}, str: ''}
-  try {
-    jsonInfo.obj = getOptions(cmd)
-    jsonInfo.str = JSON.stringify(jsonInfo.obj, null, 2)
-  } catch (error) {
-    console.log('error', error)
-  }
+
   new Promise(() => {
-    require('child_process').exec(cmd, (err, stdio, stderr) => {
-      let isHtml = stderr.includes('< content-type: text/html')
-      try {
-        stdio = JSON.stringify(JSON.parse(stdio), null, 2)
-      } catch (error) {
-        isHtml = true
-        console.log('error', error)
-      }
-      res.type('html')
-      res.send(`
-        <style>
-           body {
-             margin: 0;
-             padding: 10px;
-             background: #000;
-             color: #fff;
-           }
-           body>pre {
-             white-space: pre-wrap;
-             overflow: scroll;
-           }
-           body>pre>details>summary {
-             cursor: pointer;
-             border-bottom: 1px solid #666;
-             outline: none;
-             font-size: 14px;
-           }
-           body>pre>details>textarea {
-             font-size: 12px;
-             width: 100%;
-             height: 40vh;
-             color: #fff;
-             outline: none;
-             font-size: nomore;
-             resize: vertical;
-             border: none;
-             background: #333;
-           }
-        </style>
-        <pre>
-${jsonInfo.obj.method || 'GET'} ${jsonInfo.obj.url}
-<details>
-<summary>----- input:</summary>
-<textarea disabled spellcheck="false">${jsonInfo.str}</textarea>
-</details>
-<details open="open">
-<summary>----- out:</summary>
-${isHtml ? stdio : `<textarea disabled spellcheck="false">${stdio}</textarea>`}
-</details>
-<details>
-<summary>----- headers:</summary>
-<textarea disabled spellcheck="false">${stderr}</textarea>
-</details>
-        </pre>
-      `)
-      // res.json({
-      //   res: JSON.parse(cmdRes),
-      // })
-    })
+    let isHtml
+    let httpRes
+    let httpReq
+    try {
+      httpReq = httpHistory[api].req
+      httpRes = httpHistory[api].res
+      isHtml = httpRes.headers[`content-type`].includes(`text/html`)
+    } catch (error) {
+      console.log('error', {api, error})
+      res.send(error)
+      return
+    }
+    res.type('html')
+    res.send(`
+      <style>
+        body.api {
+          margin: 0;
+          padding: 10px;
+          background: #000;
+        }
+        body.api>.sketch {
+          color: #fff;
+        }
+        body.api>details summary {
+          cursor: pointer;
+          border-bottom: 1px solid #666;
+          outline: none;
+          font-size: 14px;
+          color: #fff;
+        }
+        body.api>details textarea {
+          font-size: 12px;
+          padding: 4px;
+          width: 100%;
+          height: 40vh;
+          color: #fff;
+          outline: none;
+          font-size: nomore;
+          resize: vertical;
+          border: none;
+          background: #333;
+        }
+        body.api>details .html {
+          width: 100%;
+          height: 100%;
+          margin: 0;
+          padding: 0;
+          border: 0;
+          outline: none;
+        }
+      </style>
+      <body class="api">
+      <span class="sketch">${api}</span>
+      <details>
+        <summary>----- input:</summary>
+        <textarea disabled spellcheck="false">${o2t(httpReq)}</textarea>
+      </details>
+      <details open="open">
+        <summary>----- out:</summary>
+        ${isHtml ? `<iframe class="html" srcdoc="${htmlEscape(httpRes.body)}"></iframe>` : `<textarea disabled spellcheck="false">${o2t(httpRes)}</textarea>`}
+      </details>
+      </body>
+    `)
   })
 })
 
@@ -170,6 +206,10 @@ function getOptions(cmd) {
     res.body = {}
   }
   return res
+}
+
+function o2t(o) {
+  return JSON.stringify(o, null, 2)
 }
 
 function getToken() {}
