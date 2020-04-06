c8499643c579b2253b71819de931f6fdf34e4e61
xw <daysme@gmail.com>
2020-04-06 14:02:17 +0800

    refactor: 删除调试数据

--- page/HttpShow.jsx
+++ page/HttpShow.jsx
@@ -28,56 +28,6 @@ window.HttpShow = (() => {
   function com() {
     // 【计数器改写方法一】 React Hooks之useContext
     // https://blog.csdn.net/weixin_44282875/article/details/85336106
-    const httpData = { // 使用 content 把请求数据传给每个组件
-      method: `GET`,
-      api: `/api/options/?page=1&pageSize=10`,
-      data: {
-        "req": {
-          "headers": {
-            "host": "localhost:9004",
-            "connection": "keep-alive",
-            "content-length": "501",
-            "accept": "application/json, text/plain, */*",
-            "sec-fetch-dest": "empty",
-            "test": "emptyemptyemptyemptyemptyemptyemptyemptyemptyemptyemptyemptyemptyemptyemptyemptyemptyemptyemptyemptyempty",
-          },
-          "body": {
-            "中文@": "你好=",
-            "receiveDate": "2020-03-31",
-            "effectiveDate": "2020-03-31",
-            "regulatorLocationId": 53,
-            "registryDeptId": 1,
-            "effectiveStatus": 2
-          },
-          "query": {
-            "中文@": "你好=",
-            "docType": "10",
-            "pageSize": "9999",
-            "docID": "32"
-          },
-          "form": {
-            "中文@": "你好="
-          },
-          "path": "/api/regulations/"
-        },
-        "res": {
-          "info": {
-            "status": 200,
-            "statusText": "OK"
-          },
-          "headers": {
-            "server": "nginx/1.13.7",
-            "date": "Tue, 31 Mar 2020 07:44:08 GMT",
-            "content-type": "application/json",
-          },
-          "body": {
-            "contentType": "application/json",
-            "extensionName": "json",
-            "bodyPath": "./httpData//api_regulations_POST_body_18.json"
-          }
-        }
-      },
-    }
 
     const [state, setState] = useState({ // 默认值
       httpData: undefined,
