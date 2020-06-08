a4d5b4d594300aaa41dde13828d7c7870e13e8cc
wll8 <xwdaysme@gmail.com>
2020-06-08 13:28:05 +0800

    refactor: 更新命令行参数解析方法

--- config.js
+++ config.js
@@ -18,10 +18,20 @@ const config = {
   ...cliArg,
 }
 
-function parseArgv() {
-  return process.argv.slice(2).reduce((acc, arg) => {
-    let [k, v = true] = arg.split('==')
-    acc[k] = v
+function parseArgv(arr) {
+  return (arr || process.argv.slice(2)).reduce((acc, arg) => {
+    let [k, v] = arg.split('==')
+    acc[k] = v === undefined // 没有值时, 则表示为 true
+      ? true
+      : (
+        /^(true|false)$/.test(v) // 转换指明的 true/false
+        ? v === 'true'
+        : (
+          /[\d|\.]+/.test(v)
+          ? (isNaN(Number(v)) ? v : Number(v)) // 如果转换为数字失败, 则使用原始字符
+          : v
+        )
+      )
     return acc
   }, {})
 }
