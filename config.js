ee6bb4956925e02875e5909956a11dcd20ec9487
xw <daysme@gmail.com>
2020-07-04 21:26:57 +0800

    refactor: 无需 config.js 中的 noProxy 配置, 存在拦截方法则进行拦截.
    更新 api.js 中的拦截器书写方式, 支持使用 [path-to-regexp](https://www.npmjs.com/package/path-to-regexp) 的方式配置拦截的 path.

--- config.js
+++ config.js
@@ -8,10 +8,6 @@ const config = {
   apiInHeader: true, // 在 header 中添加调试 api 地址, true: 是; false, 否; string: 以 string 为 header key
   proxy: 'http://httpbin.org/', // 后台服务器的的 api
   openApi: `https://httpbin.org/spec.json`, // 关联的 openApi 数据文件
-  noProxy: [ // 不进行代理的路由, 可以进行 mock 操作
-    `/image/t/*`,
-    `/status/200`,
-  ],
   dbJsonName: './db.json', // db.js 生成的 json 数据文件名
   dataDir: './httpData/', // 数据保存目录
   httpHistory: './httpData/httpHistory.json', // 录制信息保存位置
