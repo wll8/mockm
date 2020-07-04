2235d0cd1e7d0b24e9725cb5aaf45d84b67f3cfb
wll8 <xwdaysme@gmail.com>
2020-07-04 21:26:57 +0800

    refactor: 无需 config.js 中的 noProxy 配置, 存在拦截方法则进行拦截.
    更新 api.js 中的拦截器书写方式, 支持使用 [path-to-regexp](https://www.npmjs.com/package/path-to-regexp) 的方式配置拦截的 path.

--- api.js
+++ api.js
@@ -1,27 +1,29 @@
 const {mock} = require('mockjs')
 
-module.exports = server => {
-  server.post('/file/upload', (req, res, next) => { // 上传文件
+module.exports = {
+  "*" (req, res, next) { // 拦截所有方法和路由
+    next()
+  },
+  "post /file/upload" (req, res, next) { // 获取上传的文件
     const multiparty = require('multiparty')
     const form = new multiparty.Form()
     form.parse(req, (err, fields = [], files) => {
       const data = {fields, files, err}
       res.json(data)
     })
-  })
-  server.get(`/image/t/json`, (req, res, next) => {
+  },
+  "get /image/t/json" (req, res, next) { // 使用 mock 功能
     res.json(handleRes(res, {
       name: mock(`@cname`)
     }))
-  })
-  server.get(`/file`, (req, res, next) => {
+  },
+  "get /file" (req, res, next) { // 发送文件
     res.sendFile(__dirname + '/readme.md')
-  })
-  server.get(`/status/200`, (req, res, next) => {
-    res.statusCode = 300
-    res.send(`status 300`)
-  })
-  return server
+  },
+  "get /status/:code" (req, res, next) { // 使用 params 参数
+    res.statusCode = req.params.code
+    res.json(req.params)
+  },
 }
 
 function handleRes(res, data) {
