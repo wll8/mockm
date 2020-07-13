9f3de6a4f7f680541fff50655f130995c1ad8940
wll8 <xwdaysme@gmail.com>
2020-07-13 20:05:03 +0800

    fix: 修正 print 方法

--- log.js
+++ log.js
@@ -6,14 +6,14 @@ const util = require('./util.js')
 function print(...argList) { // 用于输出有用信息, 而不是调试信息
   const resList = []
   argList.map(item => {
-    const type = util.getType(item)
+    const type = util.isType(item)
     ;([
       ['undefined', ''],
       ['string', 'number', item],
       ['object', 'error', 'array', inspect(item || '', false, null, true)],
     ]).forEach(item2 => item2.slice(0, -1).includes(type) && resList.push((item2.reverse())[0]))
   })
-  console.log(...resList)
+  console._log ? console._log(...resList) : console.log(...resList)
 }
 
 function logHelper(isUse = true) { // 重写 console.log 方法, 打印时附带日期, 所在行
