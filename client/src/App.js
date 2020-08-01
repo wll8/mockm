04f057df614ed16031b399c66a0b9278ba0c1716
wll8 <xwdaysme@gmail.com>
2020-08-01 21:33:23 +0800

    feat: 在 client 中使用 antd

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Button } from 'antd';
 import './App.scss';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button type="primary">Button</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
