0f16416a5c2c15283a78729adbb508d799199e1b
wll8 <xwdaysme@gmail.com>
2020-08-20 20:48:14 +0800

    fix: 不应使用错误的客户端输出目录, 从而导致测试页面不能正确查找和访问

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,5 +1,8 @@
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 module.exports = function override(config, env) {
-  config.plugins.push(new BundleAnalyzerPlugin())
+  const showGraph = false // 是否显示依赖分析图
+  if(showGraph) {
+    config.plugins.push(new BundleAnalyzerPlugin())
+  }
   return config;
 }
