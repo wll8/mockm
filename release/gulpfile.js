65bbe4106fa08699490834ab9613bcf4d7226725
wll8 <xwdaysme@gmail.com>
2020-08-21 10:38:00 +0800

    feat: 修改输出的目录 server 为 package
    - 这是为了方便发布, 参考 `package-lock.json` 中依赖的压缩包内目录为 `package/`

--- release/gulpfile.js
+++ release/gulpfile.js
@@ -1,5 +1,4 @@
 const gulp = require(`gulp`)
-const gulpCopy = require(`gulp-copy`)
 // const rename = require(`gulp-rename`)
 const del = require(`del`)
 const uglify = require(`gulp-uglify-es`).default
@@ -8,23 +7,28 @@ gulp.task(`clear`, () => { // 复制 server 中的文件, 例如 package.json
   return del([`../dist/**`, `!../dist`], { force: true }) // 清除目录
 })
 
-gulp.task(`copyServer`, () => { // 复制 server 中的文件, 例如 package.json
-  return gulp
-    .src([
-      `../server/**/*.*`,
-      '!../server/node_modules/**',
-      '!../server/httpData/**',
-      '!../server/db.json',
-    ])
-    .pipe(gulpCopy(`../dist/server`))
+gulp.task(`copyServer`, (cb) => { // 复制 server 中的文件, 例如 package.json
+  const shell = require(`shelljs`)
+  const cmdList = `
+    npx shx mkdir -p ../dist/package/
+    npx shx cp ../server/*.* ../dist/package/
+    npx shx cp -r ../server/page ../server/util ../dist/package/
+  `.split(`\n`).map(item => item.trim()).filter(item => item)
+  cmdList.forEach(cmd => {
+    console.log(`run: ${cmd}`)
+    if(shell.exec(cmd).code !== 0) {
+      new Error(`运行错误: ${cmd}`)
+    }
+  })
+  cb()
 })
 
 gulp.task(`uglify`, () => {
   // uglify-es - https://github.com/mishoo/UglifyJS/tree/harmony
   return gulp.src([`../server/**/*.js`, '!../server/page/**', '!../server/node_modules/**'])
     .pipe(uglify())
-    .pipe(gulp.dest(`../dist/server`))
-});
+    .pipe(gulp.dest(`../dist/package`))
+})
 
 // see: https://github.com/gulpjs/gulp/issues/1091#issuecomment-163151632
 gulp.task(
