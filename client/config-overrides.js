3be460f40e23a77d1629df2b6fdf3d7627e62c09
wll8 <xwdaysme@gmail.com>
2020-08-21 20:38:05 +0800

    chore: 补充依赖

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,7 +1,7 @@
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 module.exports = function override(config, env) {
   const showGraph = false // 是否显示依赖分析图
   if(showGraph) {
+    const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
     config.plugins.push(new BundleAnalyzerPlugin())
   }
   return config;
