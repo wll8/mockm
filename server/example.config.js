fe3666231a2063345ea206d8637ee4c83595dd2f
wll8 <xwdaysme@gmail.com>
2021-07-30 18:56:07 +0800

    feat: 给包裹 api 的返回值的函数设置 code 默认值

--- server/example.config.js
+++ server/example.config.js
@@ -162,10 +162,11 @@ module.exports = util => {
   }
 }
 
-function wrapApiData({data, code}) { // 包裹 api 的返回值
+function wrapApiData({data, code = 200}) { // 包裹 api 的返回值
+  code = String(code)
   return {
     code,
-    success: Boolean(('' + code).match(/^[2]/)), // 如果状态码以2开头则为 true
+    success: Boolean(code.match(/^[2]/)), // 如果状态码以2开头则为 true
     data,
   }
 }
