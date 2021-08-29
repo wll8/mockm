04daa5c4ef45be9f995b9829116dbe0afb87fcd5
wll8 <xwdaysme@gmail.com>
2021-08-29 17:27:27 +0800

    refactor: 优化参数 --config 赋值

--- server/run.js
+++ server/run.js
@@ -25,11 +25,10 @@ const {
 } = business
 let config = {}
 const {
-  getConfigFile,
+  configFileFn,
   checkEnv,
 } = initHandle()
-const configFile = getConfigFile()
-cliArg[`--config`] = configFile
+const configFile = configFileFn({cliArg})
 const base64config = Buffer.from(JSON.stringify(cliArg)).toString('base64') // 以 base64 方式向 `node server.js` 传送命令行参数
 const os = require(`os`)
 const sharePath = path.normalize(`${os.tmpdir}/publicStore_${Date.now()}.json`) // 此文件用于 run.js 与 server.js 共享变量
