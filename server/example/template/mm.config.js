5eaae915b9a4bbd8d64d6262711d4d12a9fbf4c2
wll8 <xwdaysme@gmail.com>
2021-09-08 15:37:26 +0800

    feat: 更新模板文件

--- server/example/template/mm.config.js
+++ server/example/template/mm.config.js
@@ -15,13 +15,48 @@ module.exports = util => {
     watch: [`./api/`],
     apiWeb: `./apiWeb.json`,
     proxy: {
-      '/': `http://www.httpbin.org/`,
-      '/anything/intercept': [`origin`, `127.0.0.1`],
+      '/': `http://www.httpbin.org/`, // 后端接口主域
+      '/anything/intercept': [`origin`, `127.0.0.1`], // 修改接口返回的数据
     },
     api: {
+      // 在其他文件里的 api
       ...api(util).api,
-      '/my/api': {
-        msg: `我的 api`
+      
+      // 当为基本数据类型时, 直接返回数据, 这个接口返回 {"msg":"ok"}
+      '/api/1': {msg: `ok`},
+
+      // 也可以像 express 一样返回数据
+      '/api/2' (req, res) {
+        res.send({msg: `ok`})
+      },
+
+      // 一个只能使用 post 方法访问的接口
+      'post /api/3': {msg: `ok`},
+
+      // // 一个 websocket 接口, 会发送收到的消息
+      // 'ws /api/4' (ws, req) {
+      //   ws.on('message', (msg) => ws.send(msg))
+      // },
+
+      // 一个下载文件的接口
+      '/file' (req, res) {
+        res.download(__filename, `mm.config.js`)
+      },
+
+      // 获取动态的接口路径的参数 code
+      '/status/:code' (req, res) {
+        res.json({statusCode: req.params.code})
+      },
+
+      // 使用 mockjs 生成数据
+      '/user' (req, res) {
+        const json = util.libObj.mockjs.mock({
+          'data|3-7': [{
+            userId: `@id`,
+            userName: `@cname`,
+          }]
+        })
+        res.json(json)
       },
     },
     static: [],
