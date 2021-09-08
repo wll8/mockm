afac9143dd147700c10153c26ecdf8c186907a35
wll8 <xwdaysme@gmail.com>
2021-09-08 12:55:29 +0800

    feat: 支持引用声明文件来实现自动提示功能
    - 为了让 `server\example\template\api\index.js` 看起来简单的使用 d.ts, 修改了代码, 让它其实返回整个 mockm 配置
    - 删除了 example.config.js 中的配置项注释, 避免存在太多冗余
    - 为了保存开发和发布的代码统一, 请手动在 server 目录中运行命令 `npm run dts`

--- server/example/template/api/index.js
+++ server/example/template/api/index.js
@@ -1,16 +1,19 @@
 const { wrapApiData } = require(`../util.js`)
 
+/** @type {import('mockm/@types/config').Config} */
 module.exports = util => {
   const {
     libObj: { mockjs },
   } = util
   return {
-    // 创建接口并使用 mockjs 生成数据
-    'get /api/test': wrapApiData(mockjs.mock({
-      'data|3-7': [{
-        userId: `@id`,
-        userName: `@cname`,
-      }]
-    })),
+    api: {
+      // 创建接口并使用 mockjs 生成数据
+      'get /api/test': wrapApiData(mockjs.mock({
+        'data|3-7': [{
+          userId: `@id`,
+          userName: `@cname`,
+        }]
+      })),
+    }
   }
 }
